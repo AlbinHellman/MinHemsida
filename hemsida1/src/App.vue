9b7da4f05c79873ce20ef2816196f314ef57f2b1
Albin Hellman <steelalbin@gmail.com>
2022-05-17 22:55:14 +0200

    hemsida1

--- hemsida1/src/App.vue
+++ hemsida1/src/App.vue
@@ -1,16 +1,19 @@
 <template>
+  <MyNavbar />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import MyNavbar from './components/navbar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MyNavbar,
+    HelloWorld,
+}
 }
 </script>
 
